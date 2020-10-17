cb929046c93b727980f556f32de42d23674b659e
pchunt96 <31640912+pchunt96@users.noreply.github.com>
2020-10-16 21:00:29 -0400

    added the code to the files

--- test_llr.R
+++ test_llr.R
@@ -1,1 +1,30 @@
 test_llr
+##test_llr
+context("Check local linear regression function")
+library(testthat)
+source('llr_functions.R')
+
+n = 15
+## a very simple regression model
+x = rnorm(n)
+y = rnorm(x + rnorm(n))
+z = seq(-1, 1, length.out = 100)
+
+test_that("llr output has correct length", {
+  expect_equal(length(llr(x, y, z, omega = 1)), length(z))
+})
+
+test_that("make_weight_matrix works on simple cases", {
+  Wz <- make_weight_matrix(z[1],x,1)
+  expect_true(all(Wz[upper.tri(Wz)]==0, Wz[lower.tri(Wz)] == 0))
+  r <- abs(x-z[1])/1
+  expect_equal(diag(Wz), sapply(r,W))
+})
+
+test_that("make_predictor_matrix works on simple cases", {
+  n <-length(x)
+  X <- make_predictor_matrix(x)
+  expect_equal(dim(X),c(n,2))
+  expect_equal(X[,1],rep(1,n))
+})
+
